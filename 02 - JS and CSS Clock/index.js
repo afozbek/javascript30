cbba705658e6aa766d5d0fec0ac7a5834e7d4244
afozbek <furkanozbek1995@gmail.com>
2019-08-13 13:19:34 +0300

    final

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -19,39 +19,4 @@ const setDate = () => {
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 };
 
-// Get Angle function
-function getAngle(hand) {
-    let el = hand;
-    let st = window.getComputedStyle(el, null);
-    let tr =
-        st.getPropertyValue("-webkit-transform") ||
-        st.getPropertyValue("-moz-transform") ||
-        st.getPropertyValue("-ms-transform") ||
-        st.getPropertyValue("-o-transform") ||
-        st.getPropertyValue("transform") ||
-        "fail...";
-
-    let angle;
-    if (tr !== "none") {
-        var values = tr.split("(")[1];
-        values = values.split(")")[0];
-        values = values.split(",");
-
-        var a = values[0];
-        var b = values[1];
-
-        var radians = Math.atan2(b, a);
-
-        if (radians < 0) {
-            radians += 2 * Math.PI;
-        }
-
-        angle = Math.round(radians * (180 / Math.PI));
-    } else {
-        angle = 0;
-    }
-
-    return angle;
-}
-
 setInterval(setDate, 1000);
