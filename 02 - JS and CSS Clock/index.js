8334c34e46e206497e54430be06bf0aff231401c
afozbek <furkanozbek1995@gmail.com>
2019-08-13 13:13:12 +0300

    handling time

---
+++ 02 - JS and CSS Clock/index.js
@@ -0,0 +1,56 @@
+const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+// 450 deg full
+const setDate = () => {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hour = now.getHours();
+
+    const secondDegrees = (seconds / 60) * 360 + 90;
+    const minutesDegrees = (minutes / 60) * 360 + 90;
+    const hourDegrees = (hour / 12) * 360 + 90;
+
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+    minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+};
+
+// Get Angle function
+function getAngle(hand) {
+    let el = hand;
+    let st = window.getComputedStyle(el, null);
+    let tr =
+        st.getPropertyValue("-webkit-transform") ||
+        st.getPropertyValue("-moz-transform") ||
+        st.getPropertyValue("-ms-transform") ||
+        st.getPropertyValue("-o-transform") ||
+        st.getPropertyValue("transform") ||
+        "fail...";
+
+    let angle;
+    if (tr !== "none") {
+        var values = tr.split("(")[1];
+        values = values.split(")")[0];
+        values = values.split(",");
+
+        var a = values[0];
+        var b = values[1];
+
+        var radians = Math.atan2(b, a);
+
+        if (radians < 0) {
+            radians += 2 * Math.PI;
+        }
+
+        angle = Math.round(radians * (180 / Math.PI));
+    } else {
+        angle = 0;
+    }
+
+    return angle;
+}
+
+setInterval(setDate, 1000);
