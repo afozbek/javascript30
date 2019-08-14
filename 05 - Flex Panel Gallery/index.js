99662a74ce2b79a1a404ebd2ebb69e171d1b795a
afozbek <furkanozbek1995@gmail.com>
2019-08-14 15:54:58 +0300

    updating panels

--- 05 - Flex Panel Gallery/index.js
+++ 05 - Flex Panel Gallery/index.js
@@ -1,9 +1,18 @@
 const panels = document.querySelectorAll(".panel");
 
 function panelClickHandler() {
+    panels.forEach(panel => {
+        if (panel === this) {
+            return;
+        }
+        panel.classList.remove("open");
+    });
+
     this.classList.toggle("open");
 }
 
+/* Safari transitionend event.propertyName === flex */
+/* Chrome + FF transitionend event.propertyName === flex-grow */
 function toggleActiveHandler(e) {
     if (e.propertyName.includes("flex")) {
         this.classList.toggle("open-active");
