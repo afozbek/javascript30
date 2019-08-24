fe3df17417957f3e1b4d5e8f8205c8ebea474f3c
afozbek <furkanozbek1995@gmail.com>
2019-08-24 12:58:20 +0300

    speech synthesis

--- 23 - Speech Synthesis/index.js
+++ 23 - Speech Synthesis/index.js
@@ -5,3 +5,48 @@ const speakButton = document.querySelector("#speak");
 const stopButton = document.querySelector("#stop");
 
 let voices = [];
+msg.text = document.querySelector("[name='text']").value;
+
+function populateVoices() {
+    voices = this.getVoices();
+
+    voicesDropdown.innerHTML = voices
+        .filter(voice => voice.lang.includes("en")) // filtering english voices
+        .map(
+            voice =>
+                `<option value="${voice.name}">${voice.name}(${voice.lang})</option>`
+        )
+        .join("");
+}
+
+function setVoice() {
+    msg.voice = voices.find(voice => voice.name === this.value);
+    toggle();
+}
+
+function toggle(startOver = true) {
+    speechSynthesis.cancel();
+    if (startOver) {
+        speechSynthesis.speak(msg);
+    }
+}
+
+function setOption(e) {
+    msg[this.name] = this.value;
+    toggle();
+}
+
+function speak(e) {
+    speechSynthesis.speak(msg);
+}
+
+function stop(e) {
+    speechSynthesis.pause();
+}
+
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
+voicesDropdown.addEventListener("change", setVoice);
+options.forEach(option => option.addEventListener("change", setOption));
+
+speakButton.addEventListener("click", toggle);
+stopButton.addEventListener("click", () => toggle(false));
